92d6ccbef5a3492df946832c785590cfe29e4219
Teddy <t.boirin@icloud.com>
2020-07-08 01:55:31 +0200

    delete password hash

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -129,7 +129,7 @@ class User implements UserInterface
 
     public function setPassword(string $password): self
     {
-        $this->password = password_hash($password, PASSWORD_BCRYPT);
+        $this->password = $password;
         return $this;
     }
 
